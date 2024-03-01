10a930f9b9cb30e6cc0d6ab02af7ec1242528b4f
Joaquin Lopez <joaquin.ariel.lopez.98@gmail.com>
2024-03-01 00:26:48 -0300

    segundo commit

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const PORT = 8080
+export const MONGODB_CNX_STR = 'mongodb://127.0.0.1:27017/ecomerce';
+export const SESSION_SECRET = 'SecretCoder'
+
+export const GITHUB_APP_ID = 123456
+export const GITHUB_CLIENT_ID = 'xxxxxxxxxxxxxxxxxxxx'
+export const GITHUB_CLIENT_SECRET = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+export const GITHUB_CALLBACK_URL = 'http://localhost:8080/githubcallback'
